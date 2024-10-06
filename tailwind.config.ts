db09934d5c4abf346fd0c242b8d7066a2ff360c0
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2024-10-06 10:13:08 -0700

    install next and create layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+module.exports = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,11 +8,26 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        white: "#ffffff",
+        gray: {
+          100: "#f3f4f6",
+          200: "#e5e7eb",
+          300: "#d1d5db",
+          500: "#6b7280",
+          700: "#374151",
+          900: "#1f2937",
+        },
+        blue: {
+          200: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+        },
+        "dark-bg": "#101214",
+        "dark-secondary": "#1d1f21",
+        "blue-primary": "#0275ff",
+        "stroke-dark": "#2d3135",
       },
     },
   },
   plugins: [],
 };
-export default config;
